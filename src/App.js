3f916de09161351b8ffe984c24952048d100aaf1
lewissandwich <ots02@altisss.vn>
2021-05-29 13:50:08 +0700

    Google sheet

--- src/App.js
+++ src/App.js
@@ -1,7 +1,38 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [data, setData] = useState({
+    full_name: "Huỳnh Thế Tông", phone: '0362527648', note: 'alxtbnox__263533', date: new Date()
+  })
+
+  const handleChange = (e) => {
+    setData({ ...data, [e.target.name]: e.target.value })
+  }
+
+  const handleSubmit = async (e) => {
+    console.log('e', e)
+    e.preventDefault()
+
+    try {
+      const response = await fetch('https://v1.nocodeapi.com/anhnguoingheo/google_sheets/nBuAMJpBFtRwemST?tabId=Collection', {
+        method: 'POST',
+        headers: {
+          'Content-type': 'application/json'
+        },
+        body: JSON.stringify([[data.full_name, data.phone, data.note, data.date.toLocaleString()]])
+      })
+      
+
+      const res = await response.json()
+      console.log('Res', res)
+
+      setData({ ...data, full_name: "Huỳnh Thế Tông", phone: '0362527648', note: 'alxtbnox__263533', date: new Date() })
+    } catch (error) {
+
+    }
+  }
   return (
     <div className="App">
       <header className="App-header">
@@ -17,6 +48,7 @@ function App() {
         >
           Learn React
         </a>
+        <button onClick={handleSubmit}>Kiểm tra</button>
       </header>
     </div>
   );
